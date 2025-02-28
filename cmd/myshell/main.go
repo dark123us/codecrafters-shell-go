36d3b6d890aa2cabcd3be96b30f24b1415c83220
Vadzim Pryhodzich <7676562@gmail.com>
2025-02-28 15:31:27 +0000

    multiple command fix

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -41,7 +41,8 @@ func main() {
 				return app[0] + " ", nil
 			}
 			if compareAppPrefixes(appPrefixes, app) {
-				msg := "\n" + strings.Join(appPrefixes, "  ") + "\n" + "$ " + text
+				// msg := "\n" + strings.Join(appPrefixes, "  ") + "\n" + "$ " + text
+				msg := strings.Join(appPrefixes, "  ") + "\n" + "$ " + text
 				fmt.Fprint(os.Stdout, msg)
 				return text, errors.New("found multiple")
 			}
